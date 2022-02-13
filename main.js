8f3d7ad075d93d4ca44681cfdb17953871ec026d
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-02-13 23:00:45 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener('scroll', ()=> {
     }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
@@ -21,8 +22,15 @@ navbarMenu.addEventListener('click', (event) =>{
     if(link == null ) {
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen 
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
@@ -86,6 +94,9 @@ workBtnCotainer.addEventListener('click', (e) =>{
     },300);
     
 });
+
+
+
 
 // 프로젝트 목록 보이기 도전 내가 한 코드 
 // 가독성이 좋지 않고, 자주 출현하는 반복문
