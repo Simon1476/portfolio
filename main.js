7e0b5d701f2d66ee9f1e92cb440628a831868a99
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-02-13 16:43:59 +0900

    JS: Work filtering

--- main.js
+++ main.js
@@ -55,8 +55,73 @@ arrowBtn.addEventListener('click', ()=>{
     scrollIntoView('#home');
 });
 
+
+//Projects 
+const workBtnCotainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnCotainer.addEventListener('click', (e) =>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    
+    setTimeout(()=> {
+        projects.forEach((project) => {
+            if(filter ==='*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },300);
+    
+});
+
+// 프로젝트 목록 보이기 도전 내가 한 코드 
+// 가독성이 좋지 않고, 자주 출현하는 반복문
+// const categoryBtn = document.querySelectorAll('.category__btn');
+// const projectList = document.querySelectorAll('.project');
+// console.log(projectList[0].dataset.kind);
+// // console.log(categoryBtn);
+// // console.log(projectList);
+// for(var i=0; i < categoryBtn.length; i++){
+//     categoryBtn[i].addEventListener('click', (event)=>{
+//         const target = event.target;
+//         const kind = target.dataset.kind;
+//         if (kind === "all"){
+//             for(var i=0; i<projectList.length; i++){
+//                   projectList[i].style.display ='block';
+//             }
+//             return;
+//         }
+        
+//         for(let i=0; i < projectList.length; i++){
+//             if(kind != projectList[i].dataset.kind) {
+//                 projectList[i].style.display ='none';
+//             } else {
+//                 projectList[i].style.display ='block';
+//             }
+//         }
+        
+//     });
+// }
+
+
+// categoryBtn.addEventListener('click', (event)=> {
+//     const target = event.target;
+//     const kind = target.dataset.kind;
+//     console.log(`Kind is ${kind}`);
+// });
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
 }
+
+
 
